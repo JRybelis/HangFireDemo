e4beba82104386d12a7543ad31301f8bb8377848
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-04 16:26:38 +0300

    Enqueue a sample job

--- HangFireDemo/Startup.cs
+++ HangFireDemo/Startup.cs
@@ -55,8 +55,9 @@ namespace HangFireDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IBackgroundJobClient backgroundJobs, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
